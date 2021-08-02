99fb756c99121a0942af21ed80593604e6e2a6dd
Rabten <rabten@protonmail.com>
2021-08-02 06:24:50 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './sass/main.scss'
+import Header from './components/header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
