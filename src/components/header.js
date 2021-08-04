4f7fb6c760290c1a1ea7fc50ee5302a71d53106c
Rabten <rabten@protonmail.com>
2021-08-04 13:57:57 -0400

    working on navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,29 @@
+import logo from '../images/logo.svg'
+import arr from '../images/icon-arrow-dark.svg'
+
 function Header() {
     return (
         <div className='header'>
-            <h1>
-                header
-            </h1>
+            <nav className='navbar'>
+                <div className='divOne'>
+                    <div className='content'>
+                        <img src={logo} />
+                        <li>Product</li>
+                        <li>Company</li>
+                        <li>Connect</li>
+                        <img id='arr' src={arr} />
+                    </div>
+                </div>
+
+                <div className='divTwo'>
+                    <button className='login'>
+                        Login
+                    </button>
+                    <button className='signup'>
+                        Sign Up
+                    </button>
+                </div>
+            </nav>
         </div>
     )
 }
