bf49e90bbfa50eaf073d81693553757afcc5acaa
Rabten <rabten@protonmail.com>
2021-08-08 17:28:40 -0400

    working on main

--- src/components/body1.js
+++ src/components/body1.js
@@ -1,7 +1,60 @@
 function body1() {
     return (
         <div className='body1'>
-            
+            <h1>
+                Designed for the future
+            </h1>
+
+            <div className='divOne'>
+                <div className='content'>
+                    <div className='box one'>
+                        <h2>
+                            Introducing an extensible editor
+                        </h2>
+                        <p>
+                            <span>
+                                Blogr features an exceedingly intuitive interface which lets you focus 
+                            </span>
+                            <span>
+                                on one thing: creating content. The editor supports management of
+                            </span>
+                            <span>
+                                multiple blogs and allows easy manipulation of embeds such as images, 
+                            </span>
+                            <span>
+                                videos, and Markdown. Extensibility with plugins and themes provide 
+                            </span>
+                            <span>
+                                easy ways to add functionality or change the looks of a blog.
+                            </span>
+                        </p>
+                    </div>
+
+                    <div className='box one'>
+                        <h2>
+                            Robust content management
+                        </h2>
+                        <p>
+                            <span>
+                                Flexible content management enables users to easily move through
+                            </span>
+                            <span>
+                                posts. Increase the usability of your blog by adding customized
+                            </span>
+                            <span>
+                                categories, sections, format, or flow. With this functionality, you’re in
+                            </span>
+                            <span>
+                                full control.
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='divTwo'>
+                
+            </div>
         </div>
     )
 }
