ecb3a6d1cd19a1fe8907047efd8f4e7ea977a1d8
Rabten <rabten@protonmail.com>
2021-08-09 17:52:58 -0400

    working on mq body2

--- src/components/body2.js
+++ src/components/body2.js
@@ -6,11 +6,22 @@ function body2() {
             <img src={phone} alt='phone' />
 
             <div className='divOne'>
-                
+
             </div>
 
             <div className='divTwo'>
-
+                <div className='content'>
+                    <h1>
+                        State of the Art Infrastructure
+                    </h1>
+    
+                    <p>
+                        With reliability and speed in mind, worldwide data centers provide the 
+                        backbone for ultra-fast connectivity. 
+                        This ensures your site will load instantly, no matter where your readers 
+                        are, keeping your site competitive.
+                    </p>
+                </div>
             </div>
         </div>
     )
