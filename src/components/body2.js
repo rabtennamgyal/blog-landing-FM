6b24ee9b9745a9b72dda6629dc3a1462d0e97916
Rabten <rabten@protonmail.com>
2021-08-09 12:37:56 -0400

    created the second body

--- src/components/body2.js
+++ src/components/body2.js
@@ -1,7 +1,17 @@
+import phone from '../images/illustration-phones.svg'
+
 function body2() {
     return (
         <div className='body2'>
+            <img src={phone} alt='phone' />
 
+            <div className='divOne'>
+                
+            </div>
+
+            <div className='divTwo'>
+
+            </div>
         </div>
     )
 }
